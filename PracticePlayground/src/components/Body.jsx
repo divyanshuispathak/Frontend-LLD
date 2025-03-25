395bcb792bf0bb8871fe6d7202f2f2d6e0209c09
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-03-25 13:34:01 +0530

    improved performance by using react-query and no unnecessary api calls when navigating back

--- PracticePlayground/src/components/Body.jsx
+++ PracticePlayground/src/components/Body.jsx
@@ -1,29 +1,47 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import Shimmer from "./Shimmer";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchData = async () => {
+  const response = await fetch("https://dummyjson.com/products");
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  const data = await response.json();
+  return data.products;
+};
 
 const Body = () => {
-  const [products, setProducts] = useState(null);
+  const { data: products } = useQuery({
+    queryKey: ["products"],  // Pass as an object
+    queryFn: fetchData,  // Pass as an object
+    staleTime: 5 * 60 * 1000, // Cache for 5 minutes (prevents unnecessary re-fetch)
+  });
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch("https://dummyjson.com/products");
-      const data = await response.json();
-      setProducts(data.products);
-    } catch (error) {
-      setError(err.message);
-    }
-  };
+  // const [products, setProducts] = useState(null);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch("https://dummyjson.com/products");
+  //     const data = await response.json();
+  //     setProducts(data.products);
+  //   } catch (error) {
+  //     setError(err.message);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return !products ? (
     <div className="flex flex-wrap gap-4 mb-4 ml-4">
-      {Array(15).fill(0).map((_, index) => (
-        <Shimmer key={index} />
-      ))}
+      {Array(15)
+        .fill(0)
+        .map((_, index) => (
+          <Shimmer key={index} />
+        ))}
     </div>
   ) : (
     <div className="bg-gray-200">
--- PracticePlayground/src/components/LeftHeader.jsx
+++ PracticePlayground/src/components/LeftHeader.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const LeftHeader = () => {
   return (
     <div className='p-2 m-2'>
-        Divyanshu Watches
+        <NavLink to='/'>Divyanshu/Nykaa</NavLink>
     </div>
   )
 }
