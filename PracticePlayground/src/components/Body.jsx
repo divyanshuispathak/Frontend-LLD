0e3a1740fd2ef007b635c26ce3d8bf5b5892cfaa
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-03-26 17:01:27 +0530

    integrated memes api and removed products api

--- PracticePlayground/src/components/Body.jsx
+++ PracticePlayground/src/components/Body.jsx
@@ -3,37 +3,38 @@ import ProductCard from "./ProductCard";
 import Shimmer from "./Shimmer";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchData = async () => {
-  const response = await fetch("https://dummyjson.com/products");
-  if (!response.ok) {
-    throw new Error("Failed to fetch products");
-  }
-  const data = await response.json();
-  return data.products;
-};
+// const fetchData = async () => {
+//   const response = await fetch("https://dummyjson.com/products");
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch products");
+//   }
+//   const data = await response.json();
+//   return data.products;
+// };
+
+// const { data: products } = useQuery({
+//   queryKey: ["products"],  // Pass as an object
+//   queryFn: fetchData,  // Pass as an object
+//   staleTime: 5 * 60 * 1000, // Cache for 5 minutes (prevents unnecessary re-fetch)
+// });
 
 const Body = () => {
-  const { data: products } = useQuery({
-    queryKey: ["products"],  // Pass as an object
-    queryFn: fetchData,  // Pass as an object
-    staleTime: 5 * 60 * 1000, // Cache for 5 minutes (prevents unnecessary re-fetch)
-  });
+  const [products, setProducts] = useState(null);
 
-  // const [products, setProducts] = useState(null);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://meme-api.com/gimme/20");
+      const data = await response.json();
+      console.log("Data from API", data);
+      setProducts(data.memes);
+    } catch (error) {
+      setError(err.message);
+    }
+  };
 
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch("https://dummyjson.com/products");
-  //     const data = await response.json();
-  //     setProducts(data.products);
-  //   } catch (error) {
-  //     setError(err.message);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return !products ? (
     <div className="flex flex-wrap gap-4 mb-4 ml-4">
@@ -47,8 +48,8 @@ const Body = () => {
     <div className="bg-gray-200">
       <h2 className="text-xl font-bold mb-4 ml-4">Products</h2>
       <ul className="flex flex-wrap gap-4">
-        {products.map((product) => (
-          <ProductCard key={product.id} productData={product} />
+        {products.map((product, index) => (
+          <ProductCard key={index} productData={product} />
         ))}
       </ul>
     </div>
--- PracticePlayground/src/components/ProductCard.jsx
+++ PracticePlayground/src/components/ProductCard.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const ProductCard = ({ productData }) => {
   return (
     <div className='flex flex-col m-auto gap-2 p-4 w-64 h-42 border border-gray-300 bg-gray-100 rounded-lg shadow-md hover:shadow-lg transition-shadow'>
-      <h3 className='text-lg font-semibold' key={productData.id}>{productData.title}</h3>
+      <h3 className='text-lg font-semibold'>{productData.author}</h3>
       {/* <h3>{productData.description}</h3> */}
-      <h5>{productData.rating}</h5>
+      <h5>{productData.url}</h5>
     </div>
   );
 };
