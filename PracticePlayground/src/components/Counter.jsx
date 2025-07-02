b50072c5ddec6e6140ef8330a22cff16ca4c6923
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-07-02 18:25:04 +0530

    played with tailwind css and centering the div

--- PracticePlayground/src/components/Counter.jsx
+++ PracticePlayground/src/components/Counter.jsx
@@ -1,46 +1,15 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const incrementCounter = () => {
-    setCount((count) => count + 1);
-  };
-
-  const decrementCounter = () => {
-    setCount((count) => count - 1);
-  };
-
-  const resetCounter = () => {
-    setCount(0);
-  };
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="flex flex-col items-center space-y-4 p-6 bg-white rounded-xl shadow-lg w-64">
-        <div className="flex items-center justify-center gap-4">
-          <button
-            className="text-2xl bg-red-200 hover:bg-red-300 px-4 py-2 rounded-full transition"
-            onClick={decrementCounter}
-          >
-            ➖
-          </button>
-          <div className="text-xl font-semibold w-10 text-center">{count}</div>
-          <button
-            className="text-2xl bg-green-200 hover:bg-green-300 px-4 py-2 rounded-full transition"
-            onClick={incrementCounter}
-          >
-            ➕
-          </button>
+    <div className="bg-amber-100 h-[100vh] w-[100vw] flex justify-center items-center">
+        <div className="bg-amber-300 min-h-6/12 min-w-6/12">
+            <div className="bg-amber-800 h-1/2 w-1/2">
+            hellow
+            </div>
         </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white font-medium px-6 py-2 rounded-md transition"
-          onClick={resetCounter}
-        >
-          Reset
-        </button>
-      </div>
     </div>
-  );
+  )
 };
 
 export default Counter;
