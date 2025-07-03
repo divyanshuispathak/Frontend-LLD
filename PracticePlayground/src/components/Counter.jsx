fd6c96520d37b900a5ee945dbae502620d395891
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-07-03 18:45:53 +0530

    practices tailwind css and counter application in different ways

--- PracticePlayground/src/components/Counter.jsx
+++ PracticePlayground/src/components/Counter.jsx
@@ -1,13 +1,43 @@
 import React, { useState } from "react";
 
 const Counter = () => {
+
+  const [count, setCount] = useState(0);
+
+  const decrementCount = () => {
+    if(count === -5) {
+      setCount(5)
+      return;
+    }
+    setCount(count - 1);
+  }
+
+  const incrementCount = () => {
+    if(count === 5) {
+      setCount(-5)
+      return;
+    }
+    setCount(count + 1);
+  }
+
+  const resetCount = () => {
+    setCount(0);
+  }
+
   return (
-    <div className="bg-amber-100 h-[100vh] w-[100vw] flex justify-center items-center">
-        <div className="bg-amber-300 min-h-6/12 min-w-6/12">
-            <div className="bg-amber-800 h-1/2 w-1/2">
-            hellow
-            </div>
+    <div className="bg-amber-100 h-[100vh] flex justify-center items-center">
+      <div className="bg-amber-600 h-[40%] w-[40%] flex flex-col justify-center items-center">
+        <div className="bg-amber-400 w-3/12 flex h-3/12 justify-between items-center px-5 my-5 rounded-2xl">
+          <button className="cursor-pointer" onClick={decrementCount}>➖</button>
+          <h1>{count}</h1>
+          <button className="cursor-pointer" onClick={incrementCount}>➕</button>
         </div>
+        <div>
+        <button className="border p-2 bg-amber-100 cursor-pointer hover:bg-amber-950 hover:text-white rounded" onClick={resetCount}>Reset</button>
+        </div>
+      </div>
+
+        
     </div>
   )
 };
